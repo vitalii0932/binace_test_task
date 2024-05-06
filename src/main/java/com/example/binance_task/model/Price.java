5d03f2c3626e07a6ad9a20521cc74a5409c89799
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-06 14:16:40 +0300

    price model

---
+++ src/main/java/com/example/binance_task/model/Price.java
@@ -0,0 +1,30 @@
+package com.example.binance_task.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@IdClass(PriceId.class)
+@Table(name = "prices")
+public class Price {
+    @Id
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Symbol symbol;
+    @Id
+    private LocalDateTime time;
+
+    @Column(precision = 15, scale = 7)
+    private BigDecimal markPrice;
+    @Column(precision = 15, scale = 7)
+    private BigDecimal indexPrice;
+    @Column(precision = 15, scale = 7)
+    private BigDecimal estimatedSettlePrice;
+    @Column(precision = 15, scale = 7)
+    private BigDecimal lastFundingRate;
+    @Column(precision = 15, scale = 7)
+    private BigDecimal interestRate;
+}
