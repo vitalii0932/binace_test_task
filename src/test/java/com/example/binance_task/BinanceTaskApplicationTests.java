455eeefe30d14f22ea469ef8610dbb8328eadfe9
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-06 14:14:26 +0300

    initial commit

---
+++ src/test/java/com/example/binance_task/BinanceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binance_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
